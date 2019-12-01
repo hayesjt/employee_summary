20a569fdcec5f8753d825543cf726e739535b140
haylee tk <haylee430@gmail.com>
2019-12-01 11:35:49 -0600

    Prompt manager created/package.json added

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
+// REQUIRED PACKAGES FOR APPLICATION TO FUNCTION CORRECTLY
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-const pdf = require('html-pdf');
 
-const writeFileAsync = util.promisify(fs.writeFile);
+// FIRST MESSAGE THAT POPS UP FOR APPLICATION
+console.log("Let's Build your employee summary")
+
+// ENTIRE FUNCTION THAT RUNS THE APPLICATION 
+function runAPP(){
+    // PROMPTS THE MANAGER TO FILL OUT ALL INFORMATION
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "what is your name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "what is your manager ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "what is your email address?"
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "what is your office number?"
+        }
+    ]).then(function (answers){
+        // CONFIRM ANSWERS HAS COME INTO THIS FUNCTION 
+        console.log(answers);
+        // PROMPTING MANAGER TO FILL OUT EMPLOYEE INFORMATION BEFORE WRITING A FILE
+    })
+}
